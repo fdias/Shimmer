208753bfc9a75b837683a5ee3b6c019ee79ab3c2
Fernando Dias Silva <fernandods>
2018-05-23 10:48:29 -0300

    first commit

---
+++ app/src/main/java/com/diax/shimmer/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.diax.shimmer
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var shimmer = Shimmer()
+        shimmer.start(shimmer_text)
+
+    }
+
+}
